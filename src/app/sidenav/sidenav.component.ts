ede3ba382ba0d3b609714cb87b6b058040a42575
KasunW-duo <kasun.w@duosoftware.com>
2018-06-04 23:24:01 +0530

    Link routing muted

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MyContentService } from  '../services/mycontent.service';
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-sidenav',
-    templateUrl: './sidenav.component.html',
-    styleUrls: ['sidenav.component.scss']
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-    constructor(public myContentService: MyContentService) { }
-    myFolders = null;
+  constructor(public myContentService: MyContentService, private router: Router) { }
+  myFolders = null;
 
-    ngOnInit() {
-        this.myFolders = this.myContentService.getAllFolders();
-    }
+  ngOnInit() {
+    this.myFolders = this.myContentService.getAllFolders();
+  }
 
+  getFolders = function () {
+    this.router.navigate(['ws/dashboard']);
+  }
 }
